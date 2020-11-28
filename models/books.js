a71c82da7e7a50d5c8f296d41036c2ce6002c829
Christopher Moroney <christopher.moroney1@gmail.com>
2020-11-28 14:28:18 -0800

    test

--- models/books.js
+++ models/books.js
@@ -6,7 +6,7 @@ let BookSchema = new Schema({
     Name: String,
     Author: String,
     ISBN: String,
-    Price: String.toFixed(2)
+    Price: String
 });
 
 module.exports = mongoose.model('book', BookSchema);
